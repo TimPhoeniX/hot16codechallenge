eb2d55c2b93d38ab7916b383ee33a0e09087fcdb
Piotr Grudzień <piotr.grudzien@outlook.com>
2020-05-22 22:41:23 +0200

    V1

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ https://www.siepomaga.pl/hot16challenge
 
 ## #hot16codechallenge
 Idea is the same, but tool is different. Let's show some 16 line code!
+
+## Survive the quarantine
+Survive till the end of quarantine.
+Everyday you'll face the decision whether to stay at home or go out to get vital supplies. Can you survive?!
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,12 @@
-import kotlinx.html.dom.*; import kotlinx.html.*; import org.w3c.dom.* import kotlin.browser.window
-fun main(): Unit = with(kotlin.browser.document) { addEventListener("load", {
-    val go = getElementById("go") as HTMLButtonElement; var food = 7; var tp = 7; var days = 1; var sick = false;
-    val stay = getElementById("stay") as HTMLButtonElement; fun roll() = kotlin.random.Random.nextInt(3,7);
-    fun end() = when { food == 0 -> "You've run out of food!"; tp == 0 -> "You've run out of toilet paper!"; sick -> "You've been caught by sharp shadow of a fog!" else -> null};
-    fun day() = body!!.append.div { h1 { +(end() ?: run { window.setTimeout({ window.location.href = "https://www.youtube.com/watch?v=2c4hnA8jXwo" },500); "You've been caught by a sharp shadow of a fog"}) } }
-    go.addEventListener("click", { days++; food += roll(); tp += roll(); sick = kotlin.random.Random.nextDouble(.0,100.0) < 3.3; day() })
-    stay.addEventListener("click", { days++; food--; tp--; day() })})}
+import kotlinx.html.dom.*; import kotlinx.html.*; import org.w3c.dom.*; import kotlin.random.*
+fun main(): Unit = with(kotlin.browser.document) {
+    val go = getElementById("go") as HTMLElement; val stay = getElementById("stay") as HTMLElement;
+    var food = 7; var tp = 7; var days = 0; var sick = false; fun roll() = Random.nextInt(3,7);
+    val log = getElementById("log") as HTMLElement
+    fun end() = when { food == 0 -> "You've run out of food!"; tp == 0 -> "You've run out of toilet paper!";
+        sick -> "You've been caught by a sharp shadow of a fog!" else -> null};
+    fun day() = log.prepend.div { h1 { +(end()?.apply { with(kotlin.browser.window)
+        { setTimeout({ location.href = "https://www.youtube.com/watch?v=2c4hnA8jXwo" },2500)}; go.remove(); stay.remove()}
+        ?: "You've survived day $days".apply { if(log.childElementCount > 12) log.lastChild?.let { log.removeChild(it) } })}}
+    go.addEventListener("click", { days++; food += roll(); tp += roll(); sick = Random.nextDouble(.0,100.0) < 2.7; day() })
+    stay.addEventListener("click", { days++; food--; tp--; day() })}
